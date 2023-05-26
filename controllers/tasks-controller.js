c420bcc384434c5986f6d3cab9de451a006f142f
Francis Yvan Jubera <francisyvanjubera@gmail.com>
2023-05-26 10:16:01 +0800

    completed and tested basic CRUD route structure

--- controllers/tasks-controller.js
+++ controllers/tasks-controller.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res) => {
     res.send("all task items");
 }
 
+const getTask = (req, res) => {
+    res.json({ taskID: req.params.id });
+}
+
+const addTask = (req, res) => {
+    res.json(req.body);
+}
+
+const updateTask = (req, res) => {
+    res.send("update a single task");
+}
+
+const removeTask = (req, res) => {
+    res.send("remove a single task");
+}
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    getTask,
+    addTask,
+    updateTask,
+    removeTask
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // controller imports
-const tasks_controller = require('../controllers/tasks-controller');
+const tasks_controller = require("../controllers/tasks-controller");
 
-// this route provides all tasks
-router.route("/").get(tasks_controller.getAllTasks);
+/*  CRUD routes */
+router
+  .route("/")
+  .get(tasks_controller.getAllTasks)
+  .post(tasks_controller.addTask);
+
+router
+  .route("/:id")
+  .get(tasks_controller.getTask)
+  .patch(tasks_controller.updateTask)
+  .delete(tasks_controller.removeTask);
 
 module.exports = router;
